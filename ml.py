165d4ef27c5e72cd807e58927ef5bf865541b1b2
Nurul-Faizah <83937133+Nurul-Faizah@users.noreply.github.com>
2023-06-21 13:12:08 +0700

    Update ml.py

--- ml.py
+++ ml.py
@@ -56,21 +56,22 @@ with tab2:
                 ret, frame = cap.read()  # Membaca frame pertama dari kamera
 
                 if ret:
-                    st.image(frame, channels="BGR")
-
-                # Mengubah gambar menjadi bentuk yang sesuai untuk prediksi
-                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                img = cv2.resize(img,(128, 128))
-                img = np.array(img) / 255.0
-                img= np.expand_dims(img, axis=0)
-
-                # Melakukan prediksi menggunakan model atau tindakan lain
-                prediction = model.predict(img)
-                class_index = np.argmax(prediction[0])
-                class_name = classes[class_index]
-
-                # Menampilkan hasil prediksi
-                st.success(f"Hasil Prediksi: {class_name}") 
+                    # Mengubah gambar menjadi bentuk yang sesuai untuk prediksi
+                    img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    img = cv2.resize(img, (128, 128))
+                    img = np.array(img) / 255.0
+                    img = np.expand_dims(img, axis=0)
+            
+                    # Menampilkan gambar
+                    st.image(img, channels="RGB", use_column_width=True)
+            
+                    # Melakukan prediksi menggunakan model atau tindakan lain
+                    prediction = model.predict(img)
+                    class_index = np.argmax(prediction[0])
+                    class_name = classes[class_index]
+            
+                    # Menampilkan hasil prediksi
+                    st.success(f"Hasil Prediksi: {class_name}")
     
 
                 
