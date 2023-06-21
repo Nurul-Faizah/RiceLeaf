c4818e34decea292dd4bf6242a0f29d67d0ac12b
Nurul-Faizah <83937133+Nurul-Faizah@users.noreply.github.com>
2023-06-21 12:59:00 +0700

    Update ml.py

--- ml.py
+++ ml.py
@@ -51,45 +51,26 @@ with tab2:
         elif menu == "Camera":
             st.write("Click the camera button below.")
             if st.button('Camera'):
-                
-                # Buat objek kamera
-                cap = st.camera_input("Take a picture")
+                cap = cv2.VideoCapture(0)  # Menggunakan kamera utama
 
-                # Baca frame kamera secara berulang-ulang
-                if cap is not None:
-                        # To read image file buffer with OpenCV:
-                        bytes_data = cap.getvalue()
-                        cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-                    
-                        # Check the type of cv2_img:
-                        # Should output: <class 'numpy.ndarray'>
-                        st.write(type(cv2_img))
-                    
-                        # Check the shape of cv2_img:
-                        # Should output shape: (height, width, channels)
-                        st.write(cv2_img.shape)
-            # st.write("Click the camera button below.")
-            # if st.button('Camera'):
-            #     cap = cv2.VideoCapture(0)  # Menggunakan kamera utama
+                ret, frame = cap.read()  # Membaca frame pertama dari kamera
 
-            #     ret, frame = cap.read()  # Membaca frame pertama dari kamera
+                if ret:
+                    st.image(frame, channels="BGR")
 
-            #     if ret:
-            #         st.image(frame, channels="BGR")
+                # Mengubah gambar menjadi bentuk yang sesuai untuk prediksi
+                img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                img = cv2.resize(img,(128, 128))
+                img = np.array(img) / 255.0
+                img= np.expand_dims(img, axis=0)
 
-            #     # Mengubah gambar menjadi bentuk yang sesuai untuk prediksi
-            #     img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            #     img = cv2.resize(img,(128, 128))
-            #     img = np.array(img) / 255.0
-            #     img= np.expand_dims(img, axis=0)
+                # Melakukan prediksi menggunakan model atau tindakan lain
+                prediction = model.predict(img)
+                class_index = np.argmax(prediction[0])
+                class_name = classes[class_index]
 
-            #     # Melakukan prediksi menggunakan model atau tindakan lain
-            #     prediction = model.predict(img)
-            #     class_index = np.argmax(prediction[0])
-            #     class_name = classes[class_index]
-
-            #     # Menampilkan hasil prediksi
-            #     st.success(f"Hasil Prediksi: {class_name}") 
+                # Menampilkan hasil prediksi
+                st.success(f"Hasil Prediksi: {class_name}") 
     
 
                 
